cc7475f08ee8209cc05492e90e95439f0729aebe
lamvm <zzmyoheavenzz@gmail.com>
2021-03-23 14:09:13 +0700

    update version 1.8

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -10,57 +10,129 @@ class getBody extends StatelessWidget {
         Image.asset('assets/images/bg_pencil.jpg'),
         Column(
           children: <Widget>[
-            ListItem(
-              title: 'Trắc nghiệm',
-              icon: Icons.alarm_outlined,
-              circularPercentIndicator: CircularPercentIndicator(
-                radius: 23.0,
-                lineWidth: 2.0,
-                percent: 1.0,
-                progressColor: Colors.grey.shade300,
+            SizedBox(
+              height: 50.0,
+              child: Card(
+                child: InkWell(
+                  onTap: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: CircleAvatar(
+                              backgroundColor: Colors.orange.shade300,
+                              radius: 16,
+                              child: Icon(
+                                Icons.list_alt_outlined,
+                                size: 20.0,
+                                color: Colors.white,
+                              ))),
+                      Expanded(flex: 4, child: Text('Lộ trình')),
+                      Expanded(
+                          flex: 1,
+                          child: CircularPercentIndicator(
+                              radius: 20.0,
+                              lineWidth: 2.4,
+                              percent: 1.0,
+                              progressColor: Colors.grey.shade300)),
+                    ],
+                  ),
+                ),
               ),
-              onTap: () {
-                print('tap trac nghiem');
-              },
             ),
-            ListItem(
-              title: 'Trắc nghiệm',
-              icon: Icons.alarm_outlined,
-              circularPercentIndicator: CircularPercentIndicator(
-                radius: 23.0,
-                lineWidth: 2.0,
-                percent: 1.0,
-                progressColor: Colors.white,
+            SizedBox(
+              height: 50.0,
+              child: Card(
+                child: InkWell(
+                  onTap: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: CircleAvatar(
+                              backgroundColor: Colors.orange.shade300,
+                              radius: 16,
+                              child: Icon(
+                                Icons.alarm_outlined,
+                                size: 20.0,
+                                color: Colors.white,
+                              ))),
+                      Expanded(flex: 4, child: Text('Trắc nghiệm nhanh')),
+                      Expanded(
+                        flex: 1,
+                        child: Icon(
+                          Icons.local_fire_department_outlined,
+                          color: Colors.red,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-              onTap: () {
-                print('tap trac nghiem');
-              },
             ),
-            ListItem(
-              title: 'Chủ đề',
-              icon: Icons.folder,
-              circularPercentIndicator: CircularPercentIndicator(
-                radius: 23.0,
-                lineWidth: 2.0,
-                percent: 1.0,
-                progressColor: Colors.white,
+            SizedBox(
+              height: 50.0,
+              child: Card(
+                child: InkWell(
+                  onTap: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: CircleAvatar(
+                              backgroundColor: Colors.orange.shade300,
+                              radius: 16,
+                              child: Icon(
+                                Icons.alarm_outlined,
+                                size: 20.0,
+                                color: Colors.white,
+                              ))),
+                      Expanded(flex: 4, child: Text('Chủ đê')),
+                      Expanded(
+                        flex: 1,
+                        child: Icon(
+                          Icons.bookmark,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-              onTap: () {
-                print('tap chu de');
-              },
             ),
-            ListItem(
-              title: 'Bài kiểm tra',
-              icon: Icons.create,
-              circularPercentIndicator: CircularPercentIndicator(
-                radius: 23.0,
-                lineWidth: 2.0,
-                percent: 1.0,
-                progressColor: Colors.white,
+            SizedBox(
+              height: 50.0,
+              child: Card(
+                child: InkWell(
+                  onTap: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: CircleAvatar(
+                              backgroundColor: Colors.orange.shade300,
+                              radius: 16,
+                              child: Icon(
+                                Icons.create,
+                                size: 20.0,
+                                color: Colors.white,
+                              ))),
+                      Expanded(flex: 4, child: Text('Lộ trình')),
+                      Expanded(
+                        flex: 1,
+                        child: Icon(
+                          Icons.timeline,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-              onTap: () {
-                print('tap bai kiem tra');
-              },
             ),
           ],
         ),
